07588f148b0692995c6943e89f1ac8919102354f
johnlinph_2 <johnlinph_2@johnlinph-2deMac-mini.local>
2026-02-23 06:10:37 +0800

    🔧 移除 github action 內建的 setup-gradle 並清理語法

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// 解決某些沒有指定 buildscript 解析倉庫而找不到依賴的問題
 buildscript {
     repositories {
         google()
@@ -9,16 +8,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
-
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-}
-
-// 補上全域專案的 repository
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
