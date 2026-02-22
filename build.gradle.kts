e837fc5a6c7d8b4675fadeae01d37b35d0ce3e50
johnlinph_2 <johnlinph_2@johnlinph-2deMac-mini.local>
2026-02-22 22:45:08 +0800

    🔧 終極修復: 新增 gradle.properties 與 plugins 設定

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
